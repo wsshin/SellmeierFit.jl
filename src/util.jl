8d35bc150dff50d25c6f94c2fadc4305fc3515c7
Wonseok Shin <wsshin97@gmail.com>
2021-12-22 16:50:52 -0500

    Rename arguments used in read()

--- src/util.jl
+++ src/util.jl
@@ -1,10 +1,12 @@
-@enum DomainType WAVELENGTH FREQUENCY ENERGY
+@enum SpectralVariable WAVELENGTH FREQUENCY ENERGY
+for ins in instances(SpectralVariable); @eval export $(Symbol(ins)); end  # export all instances
+
 
 # Read a file containing λ and n as the first and second column, and return them.
 function read(uri::String;  # location of CSV file containing λ, n, and possibly k columns
-              dom_type::DomainType=WAVELENGTH,
-              dom_unit::Real=micro,  # e.g., micro for μm; tera for THz; e⁺ for eV
-              dom_col::Integer=1,  # column index of domain quantity
+              s_var::SpectralVariable=WAVELENGTH,
+              unit_prefix::Real=micro,  # e.g., micro for μm; tera for THz; e⁺ for eV
+              s_col::Integer=1,  # column index of domain quantity
               n_col::Integer=2,  # column index of refractive index
               kwargs...)  # keyword arguments for CSV.read; can be used to read .txt
     if splitpath(uri)[1]=="https:"
@@ -15,15 +17,15 @@ function read(uri::String;  # location of CSV file containing λ, n, and possibl
 
     num_col = size(df, 2)
     num_col≥2 || @error "Data at $uri contains $num_col columns, but should contain at least two columns corresponding to measurement domain λ and refractive index n."
-    dom = df[:,dom_col] .* dom_unit
+    dom = df[:,s_col] .* unit_prefix
     n = df[:,n_col]
     ε = n.^2
 
-    if dom_type==WAVELENGTH
+    if s_var==WAVELENGTH
         λ = dom
-    elseif dom_type==FREQUENCY
+    elseif s_var==FREQUENCY
         λ = c₀ ./ dom  # λ = c₀ / ν
-    else  # dom_type==ENERGY
+    else  # s_var==ENERGY
         λ = ℎc₀ ./ dom  # λ = c₀ / ν = hc₀ / hν = ℎc₀ / E
     end
 
