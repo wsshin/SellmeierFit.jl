7c8b7fc55598305f652acf3c52f6e01013cf4327
Wonseok Shin <wsshin97@gmail.com>
2021-12-22 01:15:14 -0500

    Add necessary packages

--- src/SellmeierFit.jl
+++ src/SellmeierFit.jl
@@ -1,3 +1,12 @@
 module SellmeierFit
 
+using LsqFit
+using CSV, DataFrames, Downloads  # used in util.jl
+using LinearAlgebra: norm
+using AbbreviatedTypes
+using SimpleConstants
+
+include("util.jl")
+include("fit.jl")
+
 end # module
