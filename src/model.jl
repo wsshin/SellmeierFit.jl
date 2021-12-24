666663ea4ea1e38a7d5379766a59e9cb23044eff
Wonseok Shin <wsshin97@gmail.com>
2021-12-24 15:00:02 -0500

    Minor updates in comments

--- src/model.jl
+++ src/model.jl
@@ -1,5 +1,7 @@
 export SellmeierModel, sellmeier_model
 
+# Sellmeier model representing ε(λ) = 1 + ∑ᵢ [strᵢ λ² / (λ² - λresᵢ²)].
+
 struct SellmeierModel{N}
     str::SFloat{N}  # strength of terms; typically written str in equation
     λres::SFloat{N}  # resonance wavelength of terms; typically written √λres in equation
@@ -9,7 +11,6 @@ Base.length(sm::SellmeierModel{N}) where {N} = N
 
 (sm::SellmeierModel)(λ::Real) = sellmeier_model(λ, sm.str, sm.λres)
 
-# Returns the relative permittivity ε = n² at a wavelength λ following the Sellmeier model
-# ε = 1 + ∑ᵢ [strᵢ λ² / (λ² - λresᵢ²)].
+# Returns the relative permittivity ε = n² at a wavelength λ following the Sellmeier model.
 sellmeier_model(λ::Real, str::AbsVecReal, λres::AbsVecReal) =
     1.0 + mapreduce((strᵢ,λresᵢ) -> strᵢ * λ^2 / (λ^2 - λresᵢ^2), +, str, λres)
