9585b0ec9b64e3e2492aa0ac3bc21c551607f58e
Wonseok Shin <wsshin97@gmail.com>
2021-12-24 15:34:50 -0500

    Update comments in example

--- example/RefractiveIndex_info.jl
+++ example/RefractiveIndex_info.jl
@@ -2,18 +2,18 @@ using SellmeierFit
 using SimpleConstants  # for micro = 1e-6
 using CairoMakie
 
-## Load refractive index data.
+## Load a set of measured refractive index data from https://RefractiveIndex.info.
 uri = "https://refractiveindex.info/data_csv.php?datafile=data/main/SiO2/Malitson.yml"  # link to RefractiveIndex.info's SiO₂
 (; λ, ε) = SellmeierFit.read(uri)
 
-## Fit the data to the Sellmeier equation.
+## Fit the loaded data to the Sellmeier equation.
 (; mdl) = fit_sellmeier(λ, ε)
 
-# Compare the fit parameters with those in RefractiveIndex.info's Dispersion formula section.
+## Compare the fit parameters with RefractiveIndex.info's Dispersion formula section.
 println("B = $(mdl.str)")
 println("√C = $(mdl.λres ./ micro)")
 
-## Visualize the data and Sellmeier equation; note that n = √ε.
+## Visualize the measured data and fit Sellmeier equation; note that n = √ε.
 fontsize = 20
 fig = Figure(; fontsize)
 Axis(fig[1,1], title="Refractive Index of SiO₂", xlabel="λ (µm)", ylabel="n")
