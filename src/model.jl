0bb7566eabfbfaeea1ada09cbc33686e3f7d76ef
Wonseok Shin <wsshin97@gmail.com>
2021-12-29 00:41:42 -0500

    Add comments about using ε rather than n

--- src/model.jl
+++ src/model.jl
@@ -12,5 +12,7 @@ Base.length(sm::SellmeierModel{N}) where {N} = N
 (sm::SellmeierModel)(λ::Real) = sellmeier_model(λ, sm.str, sm.λres)
 
 # Returns the relative permittivity ε = n² at a wavelength λ following the Sellmeier model.
+# The fitting equation is ε (= n²) rather than n, because ε is sometimes evaluated negative
+# during the optimization procedure; n = √ε is not defined for real negative ε.
 sellmeier_model(λ::Real, str::AbsVecReal, λres::AbsVecReal) =
     1.0 + mapreduce((strᵢ,λresᵢ) -> strᵢ * λ^2 / (λ^2 - λresᵢ^2), +, str, λres)
