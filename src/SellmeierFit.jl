3a410563d8c62422822b23b28661c503730b6cb0
Wonseok Shin <wsshin97@gmail.com>
2022-01-01 13:16:53 -0500

    Rename util.jl to read.jl

--- src/SellmeierFit.jl
+++ src/SellmeierFit.jl
@@ -1,11 +1,11 @@
 module SellmeierFit
 
 using LsqFit
-using CSV, DataFrames, Downloads  # used in util.jl
+using CSV, DataFrames, Downloads  # used in read.jl
 using AbbreviatedTypes
 using SimpleConstants
 
-include("util.jl")
+include("read.jl")
 include("model.jl")
 include("fit.jl")
 
